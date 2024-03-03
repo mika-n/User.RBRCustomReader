0773076f664bdd67adbba5bb1408c60bd8274f1e
mika-n <35538525+mika-n@users.noreply.github.com>
2024-03-04 00:59:10 +0200

    version bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.rallysimfans.hu")]
 [assembly: AssemblyProduct("User.RBRCustomReader")]
-[assembly: AssemblyCopyright("Copyright ©  2023, mika-n. www.rallysimfans.hu")]
+[assembly: AssemblyCopyright("Copyright ©  2023-2024, mika-n. www.rallysimfans.hu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
